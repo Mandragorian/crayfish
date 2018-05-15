b00680684d324085dacf90f9e61aa7ea7fd81503
mandragore <gkonstandinos@gmail.com>
2018-05-15 21:01:26 +0300

    fix random_in_unit_sphere implementation

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -8,7 +8,7 @@ pub fn random_in_unit_sphere() -> Vec3 {
     let mut gen = OsRng::new().unwrap();
     loop {
         let p = Vec3::new(gen.gen(), gen.gen(), gen.gen()) * 2. - Vec3::new(1., 1., 1.);
-        if p.squared_length() >= 1. {
+        if p.squared_length() <= 1. {
             return p
         }
     }
