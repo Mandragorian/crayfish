a2a4b45ffd57368c02a5681f5a5cd9baade045cc
mandragore <gkonstandinos@gmail.com>
2018-05-15 21:11:05 +0300

    Add defocus blur

--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -2,16 +2,22 @@ use std::f64::consts::PI;
 
 use vec3::Vec3;
 use ray::Ray;
+use common::random_in_unit_disc;
 
 pub struct Camera {
    lower_left: Vec3,
    horizontal: Vec3,
    vertical: Vec3,
-   origin: Vec3
+   origin: Vec3,
+   u: Vec3,
+   v: Vec3,
+   w: Vec3,
+   lens_radius: f64,
 }
 
 impl Camera {
-    pub fn new(lookfrom: Vec3, lookat: Vec3, vup: Vec3, vfov: f64, aspect: f64) -> Camera {
+    pub fn new(lookfrom: Vec3, lookat: Vec3, vup: Vec3, vfov: f64, aspect: f64, aperture: f64, focus_dist: f64) -> Camera {
+        let lens_radius = aperture / 2.;
         let theta = vfov * PI / 180.;
         let half_height = (theta/2.).tan();
         let half_width = aspect * half_height;
@@ -21,22 +27,28 @@ impl Camera {
         let v = Vec3::cross(&w, &u);
 
         let origin = lookfrom;
-        let lower_left = origin - u * half_width - v * half_height - w;
-        let horizontal = u * 2. * half_width;
-        let vertical = v * 2. * half_height;
+        let lower_left = origin - u * half_width * focus_dist - v * half_height * focus_dist - w * focus_dist;
+        let horizontal = u * 2. * half_width * focus_dist;
+        let vertical = v * 2. * half_height * focus_dist;
 
         Camera {
             lower_left,
             horizontal,
             vertical,
             origin,
+            u,
+            v,
+            w,
+            lens_radius,
         }
     }
 
-    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
-        Ray::new(self.origin, self.lower_left
-                              + (self.horizontal * u)
-                              + (self.vertical * v)
-                              - self.origin)
+    pub fn get_ray(&self, s: f64, t: f64) -> Ray {
+        let rd = random_in_unit_disc() * self.lens_radius;
+        let offset = self.u * rd.e[0] + self.v * rd.e[1];
+        Ray::new(self.origin + offset, self.lower_left
+                              + (self.horizontal * s)
+                              + (self.vertical * t)
+                              - self.origin - offset)
     }
 }
