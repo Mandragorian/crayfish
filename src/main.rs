a636ce2d0c03a91dcac9e7230366a7a5bde05c57
mandragore <gkonstandinos@gmail.com>
2018-05-15 21:16:41 +0300

    Create a random scene as example

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use rand::{OsRng, Rng};
 use raytracing::vec3::Vec3;
 use raytracing::ray::Ray;
 use raytracing::camera::Camera;
+use raytracing::common::rand;
 use raytracing::hitable::{World, Hitable};
 use raytracing::hitable::surfaces::sphere::Sphere;
 use raytracing::material::common::lambertian::Lambertian;
@@ -34,32 +35,73 @@ where
     Vec3::new(1., 1., 1.) * (1. - t) + Vec3::new(0.5, 0.7, 1.) * t
 }
 
+fn random_scene<'a>() -> impl Hitable<'a> {
+    const N: usize = 500;
+
+    let mut objects: Vec<Box<Hitable<'a>>> = Vec::with_capacity(N);
+
+    let big_sphere = Sphere::new(Vec3::new(0., -1000., -0.), 1000., Lambertian::new(Vec3::new(0.5, 0.5, 0.5)));
+    objects.push(Box::new(big_sphere));
+
+    for i in -11..=11 {
+        for j in -11..=11 {
+            let die = rand();
+            let center = Vec3::new((i as f64) + 0.9 * rand(), 0.2, (j as f64) + 0.9 * rand());
+
+            if (center - Vec3::new(4., 0.2, 0.)).length() > 0.9 {
+                if die < 0.8 {
+                    let sphere = Sphere::new(center, 0.2,
+                                             Lambertian::new(Vec3::new(rand().powi(2),
+                                                                       rand().powi(2),
+                                                                       rand().powi(2))));
+                    objects.push(Box::new(sphere));
+                } else if die < 0.95 {
+                    let sphere = Sphere::new(center, 0.2,
+                                             Metal::new(Vec3::new(0.5 * (1. + rand()),
+                                                                  0.5 * (1. + rand()),
+                                                                  0.5 * (1. + rand())),
+                                                        0.5 * rand()));
+                    objects.push(Box::new(sphere));
+                } else {
+                    let sphere = Sphere::new(center, 0.2,
+                                             Dielectric::new(1.5));
+                    objects.push(Box::new(sphere));
+                }
+            }
+        }
+    }
+
+    let sphere = Sphere::new(Vec3::new(0., 1., 0.), 1., Dielectric::new(1.5));
+    objects.push(Box::new(sphere));
+    let albedo = Vec3::new(0.4, 0.2, 0.1);
+    let sphere = Sphere::new(Vec3::new(-4., 1., 0.), 1., Lambertian::new(albedo));
+    objects.push(Box::new(sphere));
+    let albedo = Vec3::new(0.7, 0.6, 0.5);
+    let sphere = Sphere::new(Vec3::new(4., 1., 0.), 1., Metal::new(albedo, 0.));
+    objects.push(Box::new(sphere));
+    World::new(objects)
+}
+
 fn main() {
-    const NX: u32 = 200;
-    const NY: u32 = 100;
-    const NS: u32 = 100;
+    const NX: u32 = 1600;
+    const NY: u32 = 800;
+    const NS: u32 = 200;
 
     let R: f64 = (PI / 4.).cos();
 
     let mut gen = OsRng::new().unwrap();
 
-    let sphere1 = Sphere::new(Vec3::new(0., 0., -1.), 0.5, Lambertian::new(Vec3::new(0.1, 0.2, 0.5)));
-    let sphere4 = Sphere::new(Vec3::new(0., -100.5, -1.), 100., Lambertian::new(Vec3::new(0.8, 0.8, 0.)));
-    let sphere3 = Sphere::new(Vec3::new(1., 0., -1.), 0.5, Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.));
-    let sphere2 = Sphere::new(Vec3::new(-1., 0., -1.), 0.5, Dielectric::new(1.5));
+    let world = random_scene();
     
-    let world = World::new(vec![
-                           Box::new(sphere1),
-                           Box::new(sphere2),
-                           Box::new(sphere3),
-                           Box::new(sphere4)
-                          ]);
-
-    
-    let camera = Camera::new(Vec3::new(-2., 2., 1.),
-                             Vec3::new(0., 0., -1.),
+    let lookfrom = Vec3::new(13., 2., 3.);
+    let lookat = Vec3::new(0., 0., 0.);
+    let focus_dist = 10.;
+    let camera = Camera::new(lookfrom,
+                             lookat,
                              Vec3::new(0., 1., 0.),
-                             20., NX as f64 / NY as f64);
+                             20., NX as f64 / NY as f64,
+                             0.,
+                             focus_dist);
 
     let mut file = File::create("test.ppm").unwrap();
 
